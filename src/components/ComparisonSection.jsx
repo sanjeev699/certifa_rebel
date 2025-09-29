6a7ff9ab00a56762e23ed48b6203edd257636438
sanjeev699 <bhayya007@gmail.com>
2025-09-30 03:14:18 +0530

    Update ComparisonSection.jsx

--- src/components/ComparisonSection.jsx
+++ src/components/ComparisonSection.jsx
@@ -17,10 +17,14 @@ const ComparisonSection = () => {
       <div className="relative z-10 max-w-7xl mx-auto px-6 md:px-12">
 
         {/* Heading */}
-        <div className="text-center mb-12">
+        <div className="text-center mb-6">
           <h2 className="text-3xl md:text-4xl font-bold text-brandBlue">
             How's Certifa different than others?
           </h2>
+          <p className="text-gray-800 text-center mb-12 max-w-2xl mx-auto">
+          We don’t just rant, we are here to 
+          <span className="font-bold text-brandBlue"> Disrupt and Lead. </span> Buckle up!
+        </p>
         </div>
 
         {/* Cards */}
