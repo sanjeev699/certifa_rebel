3246c0b0f96f0c015079bfeb80018c19484e1cec
sanjeev699 <bhayya007@gmail.com>
2025-09-30 19:43:51 +0530

    Update FloatingWhatsApp.jsx

--- src/components/FloatingWhatsApp.jsx
+++ src/components/FloatingWhatsApp.jsx
@@ -4,7 +4,7 @@ import { FaWhatsapp } from "react-icons/fa";
 const FloatingWhatsApp = () => {
   return (
     <a
-      href="https://wa.me/919902914906"
+      href="https://wa.me/916366072794"
       target="_blank"
       rel="noopener noreferrer"
       className="fixed bottom-6 right-6 z-50 bg-green-500 hover:bg-green-600 text-white p-4 rounded-full shadow-lg flex items-center justify-center transition-all duration-300 transform hover:scale-110"
