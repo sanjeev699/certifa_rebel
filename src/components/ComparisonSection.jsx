4fdb6f5fe8f7c293b4e620daa8be898a636bad78
sanjeev699 <bhayya007@gmail.com>
2025-10-02 20:26:44 +0530

    Update ComparisonSection.jsx

--- src/components/ComparisonSection.jsx
+++ src/components/ComparisonSection.jsx
@@ -16,7 +16,7 @@ const ComparisonSection = () => {
 <div className="absolute top-10 left-1/4 w-48 h-48 md:w-72 md:h-72 bg-white opacity-20 rounded-full rotate-12 animate-ping-slow"></div>
 <div className="absolute bottom-20 right-1/4 w-56 h-56 md:w-80 md:h-80 bg-blue-500 opacity-15 rounded-full -rotate-6 animate-ping-slow"></div>
 <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-40 h-40 md:w-60 md:h-60 bg-blue-200 opacity-10 rounded-full rotate-45 animate-ping-slow"></div>
-<div className="absolute top-0 right-0 w-32 h-32 md:w-48 md:h-48 bg-blue-100 opacity-20 rounded-full -rotate-12 animate-ping-slow"></div>
+<div className="absolute top-0 right-0 w-32 h-32 md:w-48 md:h-48 bg-blue-600 opacity-20 rounded-full -rotate-12 animate-ping-slow"></div>
 
 
       {/* Main Content */}
