e3c369edcf2b59a3d4c7f4e8b258fc3f29979937
sanjeev699 <bhayya007@gmail.com>
2025-10-02 19:22:15 +0530

    Update SkillsKickstartSection.jsx

--- src/components/SkillsKickstartSection.jsx
+++ src/components/SkillsKickstartSection.jsx
@@ -32,9 +32,10 @@ const SkillsKickstartSection = () => {
       id="skills-kickstart"
       className="bg-gradient-to-b from-[#fde87c] via-white to-[#fde87c] py-10 md:py-12"
     >
-      <div className="max-w-7xl mx-auto px-6 md:px-12 text-center">
-        <h2 className="text-xl md:text-2xl font-bold text-[#000000] mb-8">
-          In the meantime,<br/> Jumpstart your learning with our <br/><span className="font-bold text-brandBlue"> Skills Kickstart Series</span>
+        <div className="relative z-10 max-w-7xl mx-auto px-6 md:px-12">
+
+        <h2 className="text-xl md:text-2xl text-bold text-[#000000] mb-8">
+         Jumpstart your learning with our <br/><span className="font-bold text-brandBlue"> Skills Kickstart Series</span>
         </h2>
         <p className="text-gray-700 mb-8 max-w-3xl mx-auto leading-relaxed">
           Access curated mini-courses worth <span className="font-bold text-green-600">₹18,000</span>, completely <span className="font-bold text-green-600">FREE</span>! 🚀 
